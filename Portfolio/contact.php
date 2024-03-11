efcc94cceb92327b85faab5e2b1fd14bd9b3329f
jfrese-designs <159507130+jfrese-designs@users.noreply.github.com>
2024-03-11 11:28:40 -0500

    portfolio

---
+++ Portfolio/contact.php
@@ -0,0 +1,41 @@
+
+<!doctype html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz@9..40&family=Noto+Serif+Georgian&display=swap" rel="stylesheet">
+  <link href="contact.css" rel="stylesheet">
+</head>
+<body>
+    <div class="designs-by">
+        <nav class="block1">
+            <div id="logo">J.Frese</div>
+            <div id="home"><a href="Portfolio.html">Home</a></div>
+            <div id="about"><a href="Portfolio/about.html">About</a></div>
+            <div id="resume"><a href="Portfolio/resume.html">Resume</a></div>
+            <div id="contact"><a href="Portfolio/contact.html">Contact</a></div>
+        </nav>
+        <div class="focus">
+            <article>
+                <h1>Contact J.Frese</h1>
+                <p><em>From an engineering student to inventory prodigy to IT Service Professional  and UX/UI Designer.</em></p>
+
+                <p>Send me a message if you like what you see.</p>
+                
+                <p>j.frese@jfresellc.com</p>
+                
+                <p>(913) 963-9628</p>
+                
+                <p>Kansas City, KS</p>
+                <button>See my formal Resume</button>
+            </article>
+            <aside>
+              <div class="form-wrapper"><form autocomplete="on" class="react-form-contents" novalidate="" data-success-redirect=""><div tabindex="-1"></div><div class="field-list"><fieldset class="form-item fields gSzoNnH14ZQZnmRRl8vE name" id="name-yui_3_17_2_1_1553888888520_3744"><legend id="name-yui_3_17_2_1_1553888888520_3744-field-wrapper"><div class="title"><div class="p_cAb_GDWwkuh8_Ohohu"><span>Name</span><span class="description required">(required)</span></div></div></legend><div class="field first-name" data-dynamic-strings=""><label class="emr40b854nlYeA1az_jA caption" for="name-yui_3_17_2_1_1553888888520_3744-fname-field"><div class="ZJ4s0QoQo14G6zUhRKmg"><div class="caption-text xco6UBjvCbSeqJsFpGf5">First Name</div></div></label><input aria-invalid="false" aria-required="true" autocomplete="given-name" class="hdiMwtVxRuvC7dBgScTh pbmBLiGMtD09ZsGQxjH4" id="name-yui_3_17_2_1_1553888888520_3744-fname-field" name="fname" type="text" value=""><span class="wpLzMraNq4rN2cAsOLyQ form-input-effects" aria-hidden="true"><span class="form-input-effects-border"></span></span></div><div class="field last-name" data-dynamic-strings=""><label class="emr40b854nlYeA1az_jA caption" for="name-yui_3_17_2_1_1553888888520_3744-lname-field"><div class="ZJ4s0QoQo14G6zUhRKmg"><div class="caption-text xco6UBjvCbSeqJsFpGf5">Last Name</div></div></label><input aria-invalid="false" aria-required="true" autocomplete="family-name" class="hdiMwtVxRuvC7dBgScTh pbmBLiGMtD09ZsGQxjH4" id="name-yui_3_17_2_1_1553888888520_3744-lname-field" name="lname" type="text" value=""><span class="wpLzMraNq4rN2cAsOLyQ form-input-effects" aria-hidden="true"><span class="form-input-effects-border"></span></span></div></fieldset><div class="form-item field email required" id="email-yui_3_17_2_1_1553888888520_3745" data-dynamic-strings=""><label for="email-yui_3_17_2_1_1553888888520_3745-field" class="title YIT5R1OYB39qtHybAVXQ"><div class="mEd53xaVzyllOCP9i6UA"><div class="HZaN3pKtOB3UmeTg6_qU"><span>Email</span><span class="description required">(required)</span></div></div></label><input aria-invalid="false" aria-required="true" autocomplete="false" class="hdiMwtVxRuvC7dBgScTh pbmBLiGMtD09ZsGQxjH4" id="email-yui_3_17_2_1_1553888888520_3745-field" placeholder="" type="email" value=""><span class="wpLzMraNq4rN2cAsOLyQ form-input-effects" aria-hidden="true"><span class="form-input-effects-border"></span></span></div><div class="form-item field textarea required" id="textarea-yui_3_17_2_1_1553888888520_3747" data-dynamic-strings=""><label for="textarea-yui_3_17_2_1_1553888888520_3747-field" class="title YIT5R1OYB39qtHybAVXQ"><div class="mEd53xaVzyllOCP9i6UA"><div class="HZaN3pKtOB3UmeTg6_qU"><span>Message</span><span class="description required">(required)</span></div></div></label><textarea id="textarea-yui_3_17_2_1_1553888888520_3747-field" aria-invalid="false" class="TC0a5swwrteZsfBY7ttl" placeholder=""></textarea><span class="wpLzMraNq4rN2cAsOLyQ form-input-effects" aria-hidden="true"><span class="form-input-effects-border"></span></span></div></div><div class="form-button-wrapper form-button-wrapper--align-left"><button type="submit" class="button sqs-system-button sqs-editable-button form-submit-button sqs-button-element-- none ACgz1Ajag8FwXHpitNSi"><span class="form-submit-button-label">Send</span><span class="jW4cbWRZSXiS4yI7Zsby form-submit-button-state" aria-label="Send"><span aria-hidden="true">Send</span><span aria-hidden="true"><span></span></span><span aria-hidden="true"><span></span></span></span></button></div></form></div>
+            </aside>
+        </div>
+    </div>
+
+</body>
+</html>
